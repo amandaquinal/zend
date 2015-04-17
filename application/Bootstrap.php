1ee95ebac52bfd425e430f417bb060b6c9ac78f3
amandaquinal <amandaquinal@users.noreply.github.com>
2015-04-17 09:53:11 +0800

    Hello and Goodbye Application

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -9,18 +9,26 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 	    $route = new Zend_Controller_Router_Route(':action', array(
 	        'module'     => 'default',
 	        'controller' => 'index',
+	        'action'     => 'index'
+	    ));
+
+	    $router2 = Zend_Controller_Front::getInstance()->getRouter();
+	    $route2 = new Zend_Controller_Router_Route(':action', array(
+	        'module'     => 'default',
+	        'controller' => 'index',
 	        'action'     => 'hello'
 	    ));
 	   
-	   $router2 = Zend_Controller_Front::getInstance()->getRouter();
-	   $route2 = new Zend_Controller_Router_Route(':action', array(
+	   $router3 = Zend_Controller_Front::getInstance()->getRouter();
+	   $route3 = new Zend_Controller_Router_Route(':action', array(
 	        'module'     => 'default',
 	        'controller' => 'index',
 	        'action'     => 'goodbye'
 	    ));
 
-	    $router->addRoute('hello', $route);
-	    $router2->addRoute('goodbye', $route2);
+	    $router->addRoute('index', $route);
+	    $router2->addRoute('hello', $route2);
+	    $router3->addRoute('goodbye', $route3);
 	}
 
 }
